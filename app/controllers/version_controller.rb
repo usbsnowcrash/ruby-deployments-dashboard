84a06ee7e3b6d3b3722604bb99102247ae3ae2b1
Jeffery Yeary <jyeary@lat-jyeary-mac.cb.careerbuilder.com>
2013-12-16 17:32:25 -0500

    fix all the calls

--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -13,44 +13,29 @@ class VersionController < ApplicationController
   end
 
   def dashboard
-    url = 'https://api.github.com/repos/cbdr/CB-Mobile/pulls?state=closed&base=production&access_token=15d13e3ca0aa8c8e8f5d9174d48e1b995b5d7450'
-    resp = HTTParty.get(url, :headers => {"User-Agent" => 'ruby'})
-    unfiltered_pulls = JSON.parse(resp.body)
+    unfiltered_pulls = @github.pull_requests.all(:user => 'cbdr', :repo => 'CB-Mobile', :state => 'closed', :base => 'production')
     @pulls = []
     unfiltered_pulls.each do |pull|
-      @pulls << pull if pull["merged_at"].nil? == false
+      @pulls << pull if pull.merged_at.nil? == false
     end
   end
 
   def index
-    @thispull = version_info
-    @commits = version_commits
+    @thispull = @github.pull_requests.get(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
+    @commits = @github.pull_requests.commits(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
     find_jobsearch_changes find_gemfile_changes
   end
 
   private
 
-  def version_info
-    pullurl = 'https://api.github.com/repos/cbdr/CB-Mobile/pulls/' + params['id'] + '?access_token=15d13e3ca0aa8c8e8f5d9174d48e1b995b5d7450'
-    pullresp = HTTParty.get(pullurl, :headers => {"User-Agent" => 'ruby'})
-    thispull = JSON.parse(pullresp.body)
-  end
-
-  def version_commits
-    url = 'https://api.github.com/repos/cbdr/CB-Mobile/pulls/' + params['id'] + '/commits?access_token=15d13e3ca0aa8c8e8f5d9174d48e1b995b5d7450'
-    resp = HTTParty.get(url, :headers => {"User-Agent" => 'ruby'})
-    commits = JSON.parse(resp.body)
-  end
 
   def find_gemfile_changes
-    url2 = 'https://api.github.com/repos/cbdr/CB-Mobile/pulls/' + params['id'] + '/files?access_token=15d13e3ca0aa8c8e8f5d9174d48e1b995b5d7450'
-    resp2 = HTTParty.get(url2, :headers => {"User-Agent" => 'ruby'})
-    @files = JSON.parse(resp2.body)
+    @files = @github.pull_requests.files(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
 
     patches_to_parse = []
     refs = []
     @files.each do |file|
-      patches_to_parse << file["patch"] if file["filename"] = "Gemfile"
+      patches_to_parse << file.patch if file.filename = "Gemfile"
     end
 
     patches_to_parse.each do |patch|
@@ -61,24 +46,25 @@ class VersionController < ApplicationController
 
   def find_jobsearch_changes(refs)
     dates = []
-    refs.each do |sha|
-      url3 = 'https://api.github.com/repos/cbdr/JobSearch/commits/' + sha[0].to_s + '?access_token=15d13e3ca0aa8c8e8f5d9174d48e1b995b5d7450'
-      resp3 = HTTParty.get(url3, :headers => {"User-Agent" => 'ruby'})
-      commit = JSON.parse(resp3.body)
-      dates << DateTime.parse(commit["commit"]["author"]["date"]) if commit["commit"].nil? == false
-    end
     @jobsearch_changes = []
+
+    refs.each do |sha|
+      begin
+        commit = @github.repos.commits.get(:user => 'cbdr', :repo => 'JobSearch', :sha => sha[0].to_s)
+        dates << DateTime.parse(commit["commit"]["author"]["date"]) if commit["commit"].nil? == false
+      rescue
+        #didn't find this commit
+      end
+    end
 
     if dates.count >= 2
       max_date = dates.max
       min_date = dates.min
-      pulls = @github.pull_requests.all(:user => 'cbdr', :repo => 'JobSearch', :state => 'closed')
+      pulls = @github.pull_requests.all(:user => 'cbdr', :repo => 'JobSearch', :state => 'closed',:base => 'master')
 
        pulls.each do |pull|
-        closed_date = DateTime.parse(pull["closed_at"])
-
-        hashy = {:title => pull["title"],:url => pull["html_url"],:user => pull["user"]}
-        @jobsearch_changes << hashy if closed_date <= max_date && closed_date >= min_date
+        closed_date = DateTime.parse(pull.closed_at)
+        @jobsearch_changes << pull if closed_date <= max_date && closed_date >= min_date
       end
 
     end

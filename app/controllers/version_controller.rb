c903f057590c1f8af78a6516060f4c45d965dc12
Jeffery Yeary <jyeary@lat-jyeary-mac.cb.careerbuilder.com>
2013-12-16 18:37:44 -0500

    fixing url thing real quick

--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -3,17 +3,8 @@ require 'httparty'
 require 'github_api'
 
 class VersionController < ApplicationController
-  def initialize
-    super
-    @github = Github.new do |config|
-      config.oauth_token = '15d13e3ca0aa8c8e8f5d9174d48e1b995b5d7450'
-      config.org = 'cbdr'
-      config.auto_pagination = true
-    end
-  end
-
   def dashboard
-    unfiltered_pulls = @github.pull_requests.all(:user => 'cbdr', :repo => 'CB-Mobile', :state => 'closed', :base => 'production')
+    unfiltered_pulls = github.pull_requests.all(:user => 'cbdr', :repo => 'CB-Mobile', :state => 'closed', :base => 'production')
     @pulls = []
     unfiltered_pulls.each do |pull|
       @pulls << pull if pull.merged_at.nil? == false
@@ -21,8 +12,8 @@ class VersionController < ApplicationController
   end
 
   def index
-    @thispull = @github.pull_requests.get(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
-    @commits = @github.pull_requests.commits(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
+    @thispull = github.pull_requests.get(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
+    @commits = github.pull_requests.commits(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
     find_jobsearch_changes find_gemfile_changes
   end
 
@@ -30,7 +21,12 @@ class VersionController < ApplicationController
 
 
   def find_gemfile_changes
-    @files = @github.pull_requests.files(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
+    #@files = Rails.cache.fetch("pulls/#{params['id']}/files", :expires_in => 12.hours) do
+    #  github.pull_requests.files(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
+    #end
+
+
+    @files = github.pull_requests.files(:user => 'cbdr', :repo => 'CB-Mobile', :number => params['id'])
 
     patches_to_parse = []
     refs = []
@@ -50,7 +46,7 @@ class VersionController < ApplicationController
 
     refs.each do |sha|
       begin
-        commit = @github.repos.commits.get(:user => 'cbdr', :repo => 'JobSearch', :sha => sha[0].to_s)
+        commit = github.repos.commits.get(:user => 'cbdr', :repo => 'JobSearch', :sha => sha[0].to_s)
         dates << DateTime.parse(commit["commit"]["author"]["date"]) if commit["commit"].nil? == false
       rescue
         #didn't find this commit
@@ -60,7 +56,7 @@ class VersionController < ApplicationController
     if dates.count >= 2
       max_date = dates.max
       min_date = dates.min
-      pulls = @github.pull_requests.all(:user => 'cbdr', :repo => 'JobSearch', :state => 'closed',:base => 'master')
+      pulls = github.pull_requests.all(:user => 'cbdr', :repo => 'JobSearch', :state => 'closed',:base => 'master')
 
        pulls.each do |pull|
         closed_date = DateTime.parse(pull.closed_at)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,4 +24,5 @@ Searchtester::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.cache_store = :memory_store, { size: 64.megabytes }
 end

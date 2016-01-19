a8619af476d826284fbb5da3ac39867e7451ac62
Jeffery Yeary <jeff@debug.ninja>
2016-01-19 14:01:57 -0500

    make it precompile assets for opsworks

---
+++ deploy/before_restart.rb
@@ -0,0 +1,28 @@
+node[:deploy].each do |application, deploy|
+  if application.to_s == 'deployments-dashboard' || application.to_s == 'deployments_dashboard'
+    release_dir  = ::File.expand_path(__FILE__).sub('deploy/before_restart.rb', '')
+    Chef::Log.info("release_dir: #{release_dir}")
+    user, group  = deploy[:user], deploy[:group]
+    rails_env    = deploy[:custom_env_vars][:RACK_ENV]
+
+    execute 'Ensuring bundle is installed without test and development gems, jobs = 2' do
+      cwd release_dir
+      command 'bundle install --without development test --jobs 2'
+    end
+
+    execute "Precompiling assets for env: #{rails_env}" do
+      cwd release_dir
+      command "RAILS_ENV=#{rails_env} bundle exec rake assets:precompile"
+    end
+
+    execute "Migrating Database for env: #{rails_env}" do
+      cwd release_dir
+      command "RAILS_ENV=#{rails_env} bundle exec rake db:migrate"
+    end
+
+    execute "Ensuring app log is owned by #{user}:#{group}" do
+      cwd ::File.join(release_dir, 'log')
+      command "sudo chown #{user}:#{group} #{rails_env}.log"
+    end
+  end
+end

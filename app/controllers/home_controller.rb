97a1249110f33c7e41d73a4cc8d84b45c5f354dc
Jeffery Yeary <jeff@debug.ninja>
2016-01-19 18:32:21 -0500

    Fleshing out home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,18 @@
-require 'parallel'
 class HomeController < ApplicationController
   skip_before_filter :auth_check
   def index
-
+    @teams = github_teams if session[:token].present?
   end
+
+  def github_teams
+    github_api.orgs.teams.list
+  end
+
+  def github_api
+    Github.new do |config|
+      config.oauth_token = session[:token]
+      config.auto_pagination = true
+    end
+  end
+
 end

86830c520539980d4443eba148ef087d762c7a20
Jeffery Yeary <jeff@debug.ninja>
2016-03-16 14:07:42 -0400

    count prs not commits

--- app/controllers/deployments_controller.rb
+++ app/controllers/deployments_controller.rb
@@ -6,6 +6,11 @@ class DeploymentsController < ApplicationController
     @pulls = merged_pull_requests
     begin
       @diff = github_api.repos.commits.compare(user: params[:user_name], repo: params[:repo_name], base: 'master', head: 'production')
+      prs_behind = 0
+      @diff[:commits].each do |commit|
+        prs_behind += 1
+      end
+      @behind_by = prs_behind
     rescue Github::Error::NotFound
       @diff = nil
     end

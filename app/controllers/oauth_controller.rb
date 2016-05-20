5ac4fbcdc619402278a1c3171df8a3084e795386
Jeffery Yeary <jeff@debug.ninja>
2016-05-20 12:31:13 -0400

    redirect back to the url you were trying to hit on login

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -2,7 +2,6 @@ class OauthController < ApplicationController
   skip_before_filter :auth_check
 
   def authorize
-    reset_session
     redirect_to github_api.authorize_url(redirect_uri: oauth_token_url, scope: 'user, repo')
   end
 
@@ -10,8 +9,9 @@ class OauthController < ApplicationController
     @token = github_api.get_token(params[:code])
     @user = github_api.users.get(oauth_token: @token.token)
     @teams = github_api.orgs.teams.list(oauth_token: @token.token)
+    url = determine_next_url
     build_session
-    redirect_to root_url
+    redirect_to url
   rescue OAuth2::Error
     flash[:notice] = 'Error during login'
     redirect_to oauth_destroy_path
@@ -19,10 +19,25 @@ class OauthController < ApplicationController
 
   def destroy
     session.clear
+    session[:next] = params[:next]
     redirect_to root_url
   end
 
   private
+
+  def clear_session
+    next_url = session[:next]
+    reset_session
+    session[:next] = next_url unless next_url.present?
+  end
+
+  def determine_next_url
+    if session[:next].present?
+      session[:next]
+    else
+      root_url
+    end
+  end
 
   def build_session
     session[:token] = @token.token
@@ -33,5 +48,6 @@ class OauthController < ApplicationController
     @teams.each do |team|
       session[:teams] << team[:id]
     end
+    session.delete(:next)
   end
 end
